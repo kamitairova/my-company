a4ac576c78ac2bf005d3e6a4351cc744df49659a
Kamilla <kamilla070703@gmail.com>
2022-08-29 22:17:22 +0600

    refactoring

---
+++ script.js
@@ -0,0 +1,10 @@
+// menu.onclick = function myFunction() {
+//     var x = document.getElementById('MyHeader');
+
+//     if (x.className === "header__links") {
+//         x.className += "responsive";
+//     }
+//      else {
+//         x.className = "header__links";
+//      }
+// }
